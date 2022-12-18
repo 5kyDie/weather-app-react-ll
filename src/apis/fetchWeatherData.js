97739f70d748df7b6e442fbcb803c96c6cc2f5a9
5kyDie <dieskyking@gmail.com>
2022-12-18 19:13:13 +0400

    Refactor fetch method

--- src/apis/fetchWeatherData.js
+++ src/apis/fetchWeatherData.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-export const fetchWeatherData = (location) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lat={lat}&lon={lon}&appid=85147c8030c85790a45b4ef95fd9f101`;
-  return axios.get(url);
-};
+export const fetchWeatherData = (location) =>
+  axios.get("https://api.openweathermap.org/data/2.5/weather", {
+    params: {
+      q: location,
+      units: "metric",
+      appid: "85147c8030c85790a45b4ef95fd9f101",
+    },
+  });
